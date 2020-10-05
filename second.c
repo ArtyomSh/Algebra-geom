95d98511fbcf3ce545567ed76c9755aefb4ed891
ArtyomSh <71187863+ArtyomSh@users.noreply.github.com>
2020-10-05 18:06:06 +0400

    Update second.c

--- second.c
+++ second.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <malloc.h>
-#define vec 3
+#define vec 3 
+
 int main() {
 	int multi = 0, rang = 0;
 	int a_c = 0, b_c = 0, c_c = 0;
@@ -10,7 +11,7 @@ int main() {
 	int* c = (int*)malloc(vec * sizeof(int));
 	//ввод
 	printf(" input your vector a (x y z) :\n");
-	scanf_s("%d %d %d", &a[0],&a[1],&a[2]);
+	scanf_s("%d %d %d", &a[0], &a[1], &a[2]);
 	printf(" input your vector ab (x y z) :\n");
 	scanf_s("%d %d %d", &b[0], &b[1], &b[2]);
 	printf("input your vector c (x y z) :\n");
@@ -22,11 +23,11 @@ int main() {
 	}
 	else printf("NO kompl\n");
 	//коллинеарность
-	if (((a[1] * b[2] - a[2] * b[1]) == 0) && ((a[0] * b[2] - a[2] * b[0]) ==0) && ((a[0] * b[1] - a[1] * b[0]) ==0)) {
+	if (((a[1] * b[2] - a[2] * b[1]) == 0) && ((a[0] * b[2] - a[2] * b[0]) == 0) && ((a[0] * b[1] - a[1] * b[0]) == 0)) {
 		printf("VEC A, B - collinearn\n");
 		a_c = 1;
 	}
-	if (((a[1] * c[2] - a[2] * c[1]) == 0) && ((a[0] * c[2] - a[2] * c[0]) == 0) && ((a[0] * c[1] - a[1] * c[0]) ==0)) {
+	if (((a[1] * c[2] - a[2] * c[1]) == 0) && ((a[0] * c[2] - a[2] * c[0]) == 0) && ((a[0] * c[1] - a[1] * c[0]) == 0)) {
 		printf("VEC A, C - collinearn\n");
 		b_c = 1;
 	}
@@ -36,17 +37,23 @@ int main() {
 	}
 	// поиск ранга системы
 	rang = a_c + b_c + c_c;
+	if (a[0] == 0 && a[1] == 0 && a[2] == 0 && b[0] == 0 && b[1] == 0 && b[2] == 0 && c[0] == 0 && c[1] == 0 && c[2] == 0) {
+		printf("Rang = 0\n");
+		return 0;
+	}
 	if (rang == 3) {
 		printf("Rang = 1\n");
+		return 0;
 	}
-	if (rang == 2) {
+	if (rang == 2 || (multi ==0 && rang == 0) || rang ==1) {
 		printf("Rang = 2\n");
+		return 0;
 	}
+	
 	if (rang == 0) {
 		printf("Rang = 3\n");
+		return 0;
 	}
-	if (a[0] ==0 && a[1] ==0 && a[2] == 0 &&  b[0] ==0 &&  b[1] == 0 && b[2] == 0 &&  c[0] == 0 && c[1] == 0 && c[2] == 0) {
-		printf("Rang = 0\n");
-	}
+	
 	return 0;
 }
